c98f3294678ffc25ca96e55faa83c2a34b975786
huybng <noname210603@gmail.com>
2024-06-28 10:32:55 +0700

    fix api files and start adding models, adapters, services, with modifications of some files

---
+++ AdminApp_Test/app/src/main/java/com/admin/babyvaccinationtracker/Adapter/CustomerAdapter.java
@@ -0,0 +1,109 @@
+package com.admin.babyvaccinationtracker.Adapter;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.admin.babyvaccinationtracker.R;
+import com.admin.babyvaccinationtracker.SendNotificationActivity;
+import com.admin.babyvaccinationtracker.model.Customer;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerAdapter extends RecyclerView.Adapter<CustomerAdapter.CustomerViewHolder> {
+    private List<Customer> customerList;
+    private List<String> selectedCustomers = new ArrayList<>();
+
+    public CustomerAdapter(List<Customer> customerList) {
+        this.customerList = customerList;
+        this.selectedCustomers = SendNotificationActivity.selectedCustomers;
+    }
+
+    @NonNull
+    @Override
+    public CustomerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.customer_item, parent, false);
+        return new CustomerViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomerViewHolder holder, int position) {
+        Customer customer = customerList.get(position);
+        holder.bind(customer);
+    }
+
+    @Override
+    public int getItemCount() {
+        return customerList.size();
+    }
+
+    public class CustomerViewHolder extends RecyclerView.ViewHolder {
+        private TextView nameTextView;
+        private TextView emailTextView;
+        private TextView phoneTextView;
+        private TextView addressTextView;
+
+        ImageView customerImage;
+
+        public CustomerViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameTextView = itemView.findViewById(R.id.nameTextView);
+            emailTextView = itemView.findViewById(R.id.emailTextView);
+            phoneTextView = itemView.findViewById(R.id.phoneTextView);
+            addressTextView = itemView.findViewById(R.id.addressTextView);
+            customerImage = itemView.findViewById(R.id.customerImage);
+        }
+
+        public void bind(Customer customer) {
+            Log.i("Adaper", "bind: " + customer.toString());
+            nameTextView.setText(customer.getCus_name());
+            emailTextView.setText(customer.getCus_email());
+            phoneTextView.setText(customer.getCus_phone());
+            addressTextView.setText(customer.getCus_address());
+
+            if (selectedCustomers.contains(customer.getCustomer_id())) {
+                // Thay đổi giao diện người dùng (ví dụ: thay đổi màu nền)
+                itemView.setBackgroundColor(itemView.getContext().getResources().getColor(R.color.primarySelectedColor));
+            } else {
+                // Reset giao diện người dùng (ví dụ: trả về màu nền mặc định)
+                itemView.setBackgroundColor(itemView.getContext().getResources().getColor(android.R.color.white));
+            }
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (selectedCustomers.contains(customer.getCustomer_id())) {
+                        selectedCustomers.remove(customer.getCustomer_id());
+                    } else {
+                        selectedCustomers.add(customer.getCustomer_id());
+                    }
+                    notifyItemChanged(getAdapterPosition());
+                }
+            });
+
+            String imageUrl = customer.getCus_avatar();
+            imageUrl = imageUrl.contains("https") ? imageUrl : imageUrl.replace("http", "https");
+            Picasso.get().load(imageUrl).into(customerImage, new Callback() {
+                @Override
+                public void onSuccess() {
+
+                }
+
+                @Override
+                public void onError(Exception e) {
+                    customerImage.setImageResource(R.drawable.user_default_avatar);
+                }
+            });
+        }
+    }
+}
+
